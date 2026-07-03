bf052917675712896b465b7c56af65af0818a34a
Ville Valkila <ville.valkila@qvantel.com>
2026-07-03 14:38:30 +0300

    add work machine configs (hypr, nvim, waybar, kitty, mako, wofi, fastfetch)

---
+++ work/hypr/scripts/powermenu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+chosen=$(printf "  Shutdown\n  Restart\n  Logout" | wofi --dmenu --prompt "Power" --width 200 --height 200 --lines 3 --hide-search)
+
+case "$chosen" in
+    "  Shutdown")
+        systemctl poweroff
+        ;;
+    "  Restart")
+        systemctl reboot
+        ;;
+    "  Logout")
+        loginctl terminate-session "$XDG_SESSION_ID"
+        ;;
+esac
